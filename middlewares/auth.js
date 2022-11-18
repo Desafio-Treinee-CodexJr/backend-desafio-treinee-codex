072e2755750e591bd6eec3fcc6b734d47bdc30ed
Samuel Lucas <samuellucasvmcontato@gmail.com>
2022-11-18 18:57:38 -0300

    refactor: creating new logic for authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,40 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
+const Users = require('../model/User');
 
 const auth = (req, res, next) => {
-    const token_header = req.headers.auth;
-    if(!token_header) return res.status(401).send({ error: 'Token não enviado!' });
+    const authHeader = req.headers.authorization;
 
-    jwt.verify(token_header, config.jwt_pass, (err, decoded) => {
-        if(err) return res.status(401).send({ error: 'Token Inválido!' });
-        res.locals.auth_data = decoded;
+    if (!authHeader)
+        return res.status(401).send({ error: 'Sem token irmão' });
+
+    const parts = authHeader.split(' ');
+
+    if (!parts.length === 2)
+        return res.status(401).send({ error: 'Erro de token' });
+
+    const [scheme, token] = parts;
+
+    if (!/^Bearer$/i.test(scheme))
+        return res.status(401).send({ error: 'Token mal formatado' });
+
+    jwt.verify(token, process.env.JWT_PASSWORD_PROD, async (err, decoded) => {
+        if (err)
+            return res.status(401).send({ error: 'Token inválido' });
+
+        const user = await Users.findOne({ _id: decoded.id });
+
+        if (!user)
+            return res.status(404).send({ error: 'Usuário não existe!' });
+
+        // const token_list = user.token_list;
+        // if (!token_list.includes(token)) {
+        //     return res.status(401).send({ error: 'Token inválido' });
+        // }
+
+        req.user = user;
         return next();
-    });
+    })
 }
 
 module.exports = auth;
