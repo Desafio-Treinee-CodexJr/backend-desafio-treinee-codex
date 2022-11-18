375c6a8c8a1f797b3bcd3d0524f767b38f5b6c2f
Samuel Lucas <samuellucasvmcontato@gmail.com>
2022-11-18 19:00:07 -0300

    feat: creating task creator route

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-//const auth = require('../middlewares/auth');
+const jwt = require('jsonwebtoken');
+const auth = require('../middlewares/auth');
+const Task = require('../model/Task');
+
 
 //router.get('/', auth, (req, res) => {
 //    console.log(res.locals.auth_data);
@@ -14,6 +17,22 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     return res.send({ message: 'Tudo ok com o POST da raiz!' });
+});
+
+router.post('/task', auth ,async (req, res) => {
+    const { text } = req.body;
+    
+    if (!text) return res.status(400).send({ error: 'Necessário adicionar texto na task!' });
+
+    try {
+        if (await Task.findOne({ text })) return res.status(400).send({ error: 'Task ja existe!' });
+
+        const task = await Task.create(req.body);
+        return res.status(201).send({task});
+    }
+    catch (err) {
+        return res.status(500).send({ error: 'Erro ao cadastrar task!' });
+    }
 });
 
 module.exports = router;
